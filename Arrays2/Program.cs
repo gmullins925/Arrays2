a9f36f0474686c81e70016fe0013362bc165d4af
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-12 10:46:41 -0500

    Commit3

--- Arrays2/Program.cs
+++ Arrays2/Program.cs
@@ -27,6 +27,41 @@ namespace Arrays2
 			bool ignoreCase = name.Equals("EDGE tech ACADemy", StringComparison.CurrentCultureIgnoreCase);
 			Console.WriteLine("Can I compare and ignore case? " + (ignoreCase ? "yup" : "nope"));
 
+			int eIndex = name.IndexOf("e");
+			while (eIndex >= 0)
+			{
+				Console.WriteLine("Found 'e' at: " + eIndex);
+				eIndex = name.IndexOf("e", eIndex + 1);
+			}
+			string newString = name.Insert(8, ">HELLO!<");
+			Console.WriteLine(newString);
+			Console.WriteLine("");
+
+			int dIndex = name.LastIndexOf('d');
+			Console.WriteLine("Found last 'd' at: " + dIndex);
+
+			string[] aNames = name.Split(' ');
+			foreach (string str in aNames)
+			{
+				string padLeft = str.PadLeft(15, '.');
+				string padRight = str.PadRight(15, '_');
+				Console.WriteLine("Pad Left ->" + padLeft);
+				Console.WriteLine("Pad Right ->" + padRight);
+
+				Console.WriteLine("Unpad " + padLeft.Trim('.'));
+				Console.WriteLine("Unpad " + padRight.Trim('_'));
+			}
+
+			string nickName = name.Remove(4);
+			Console.WriteLine(nickName);
+
+			Console.WriteLine("Chop characters out of the middle: " + name.Substring(7, 8));
+
+			Console.WriteLine("Look Ma! No 'e's! " + name.Replace('e', '_'));
+
+			Console.WriteLine("UPPER CASE: " + name.ToUpper());
+			Console.WriteLine("lower case: " + name.ToLower());
+
 			Console.ReadLine();
 		}
 	}
