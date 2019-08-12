893c681ed3d4b1f8017450de2fd80a4fff008e9d
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-12 10:29:48 -0500

    Commit

---
+++ Arrays2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "Edge Tech Academy";
+			Console.WriteLine("My name is '" + name + "' and it is " + name.Length + " characters long");
+
+			bool hasTech = name.Contains("Tech");
+			Console.WriteLine("Does my name contain Tech? " + (hasTech ? "Why yes is does!" : "No Tech :-("));
+
+			bool isBoring = name.Contains("boring stuff");
+			Console.WriteLine("Does my name contain 'boring stuff'? " + (isBoring ? "Not So!" : "All exciting stuff"));
+
+			bool ending = name.EndsWith("h Academy");
+			Console.WriteLine("Does my name end with 'h Academy'? " + (ending ? "yup" : "nope"));
+
+			bool starting = name.StartsWith("Edge");
+			Console.WriteLine("Does my name start with 'Edge'? " + (starting ? "yup" : "nope"));
+
+			bool doTheyMatch = name.Equals("EDGE tech ACADemy");
+			Console.WriteLine("Are they equal? " + (doTheyMatch ? "yup" : "nope"));
+
+			bool ignoreCase = name.Equals("EDGE tech ACADemy", StringComparison.CurrentCultureIgnoreCase);
+			Console.WriteLine("Can I compare and ignore case? " + (ignoreCase ? "yup" : "nope"));
+
+			Console.ReadLine();
+		}
+	}
+}
